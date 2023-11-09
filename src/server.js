8e32ea50825557acfb793503f4d0286235e76c9d
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-11-09 14:59:28 -0300

    feat: creat

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'; // Serve para escutar as requisições
+import { config } from 'dotenv'; // Serve para ler o arquivo .env
+
+config(); // lê o arquivo .env
+
+const port = process.env.PORT || 5000; // Pega a porta do arquivo .env ou usa a porta 5000
+
+const app = express(); // Cria o servidor
+app.use(express.json()); // Faz o servidor entender JSON
+
+app.get('/', (req, res) => {
+    // Rota inicial
+  res.status(200).json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+    // Inicia o servidor
+  console.log(`Server started on http://localhost:${port}`);
+});
